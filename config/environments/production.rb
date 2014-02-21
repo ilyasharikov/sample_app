0635b82358a3d95a3feba7f475907fa1b655a526
Ilya Sharikov <ilyasharikov@gmail.com>
2014-02-20 17:13:15 -0800

    modify production env for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ SampleApp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Heroku requires false... before pushing to Heroku, run following at the command line:
+  #    ENV=production bundle exec rake assets:precompile
+  config.assets.initialize_on_precompile = false
 end
